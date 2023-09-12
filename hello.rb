19628271693e814f7f84419a0af84337c0bbe330
Riya7789 <cresthariya7@gmail .com>
2023-09-12 16:21:55 +0545

    my commit

--- hello.rb
+++ hello.rb
@@ -2,12 +2,12 @@
 puts "Hello World!"
 
 puts "I'm ready to become a ruby developer!"
-#puts "Hello, Riya Shrestha"
+#puts "Hello, Friend"
 #puts "What a wonderful day"
 #puts "I'm learning Ruby right now"
 
 
-puts 'I am from Bouddha'
+puts 'I am from Ktm'
 
 
 
